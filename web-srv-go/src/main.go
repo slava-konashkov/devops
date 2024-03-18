c0e9b11d44b4c53f1522a5f722f0c64097d705fa
ViacheslavKonashkov <slava.konashkov@gmail.com>
2024-03-18 10:28:12 +0100

    go srv

---
+++ web-srv-go/src/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// func helloWorld(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintf(w, "Hello, World!")
+// }
+
+func main() {
+	// http.HandleFunc("/", helloWorld)
+	const port = "8080"
+	fmt.Println("Server starting on port " + port + "...")
+	srv := http.FileServer(http.Dir("./html"))
+	if err := http.ListenAndServe(":"+port, srv); err != nil {
+		panic(err)
+	}
+}
